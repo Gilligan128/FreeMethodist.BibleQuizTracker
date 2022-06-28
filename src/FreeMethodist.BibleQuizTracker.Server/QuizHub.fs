8a0983dbcbc5c8aa2820af26460c44875649a424
Mark Henke <gilligan128@gmail.com>
2022-06-27 21:20:55 -0400

    Getting sample signalr connection in Bolero setup. Just the Hub for now.

---
+++ src/FreeMethodist.BibleQuizTracker.Server/QuizHub.fs
@@ -0,0 +1,17 @@
+﻿module FreeMethodist.BibleQuizTracker.Server.QuizHub
+
+open Microsoft.AspNetCore.SignalR
+open Types
+open System.Threading.Tasks
+
+type QuizRoomCLient =
+    abstract member EnteredQuiz: quizzer:Quizzer -> Task
+    abstract member Jumped: Quizzer -> Task
+
+type QuizHub () =
+    inherit Hub<QuizRoomCLient> ()
+    
+    member this.EnterQuiz (msg:QuizCode*Quizzer) =
+       let (quizCode, quizzer) = msg
+       this.Groups.AddToGroupAsync(this.Context.ConnectionId, quizCode) |> ignore
+       this.Clients.Group(quizCode).EnteredQuiz(quizzer)
