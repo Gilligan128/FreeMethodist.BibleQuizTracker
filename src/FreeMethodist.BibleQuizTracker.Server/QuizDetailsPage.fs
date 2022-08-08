69273e5098df5f2bc4898a034e58ffe29fb00961
Mark Henke <gilligan128@gmail.com>
2022-08-07 23:09:02 -0400

    added Quiz status to Details.

--- src/FreeMethodist.BibleQuizTracker.Server/QuizDetailsPage.fs
+++ src/FreeMethodist.BibleQuizTracker.Server/QuizDetailsPage.fs
@@ -25,37 +25,36 @@ let loadRunningTeam (team: QuizTeamState) : ItemizedTeam =
       Quizzers = team.Quizzers |> List.map (fun q -> q.Name) }
 
 let loadRunningQuiz quiz =
-        { NumberOfQuestions = quiz.Questions |> Map.keys |> Seq.max
-          QuestionsWithEvents =
-            quiz.Questions
-            |> Map.map (fun k v -> (v.AnswerState, v.FailedAppeal))
-            |> ItemizedScoreModel.refreshQuestionScores
-          CompetitionStyle =
-            ItemizedCompetitionStyle.Team(quiz.TeamOne |> loadRunningTeam, quiz.TeamTwo |> loadRunningTeam) }
+    { NumberOfQuestions = quiz.Questions |> Map.keys |> Seq.max
+      QuestionsWithEvents =
+        quiz.Questions
+        |> Map.map (fun k v -> (v.AnswerState, v.FailedAppeal))
+        |> ItemizedScoreModel.refreshQuestionScores
+      CompetitionStyle = ItemizedCompetitionStyle.Team(quiz.TeamOne |> loadRunningTeam, quiz.TeamTwo |> loadRunningTeam) }
 
 let loadCompletedTeam (team: CompletedTeam) : ItemizedTeam =
     { Name = team.Name
       Quizzers = team.Quizzers |> List.map (fun q -> q.Name) }
 
 let loadCompletedQuiz (quiz: CompletedQuiz) =
-        { NumberOfQuestions =
-            quiz.CompletedQuestions.Length
-            |> PositiveNumber.numberOrOne
-          QuestionsWithEvents =
-            quiz.CompletedQuestions
-            |> List.indexed
-            |> List.collect (fun (i, v) ->
-                ItemizedScoreModel.refreshQuestionScore
-                    (i + 1 |> PositiveNumber.numberOrOne)
-                    ((QuizAnswer.Complete v.AnswerState), v.FailedAppeal))
-          CompetitionStyle =
-            match quiz.CompetitionStyle with
-            | CompletedCompetitionStyle.Team (teamOne, teamTwo) ->
-                ItemizedCompetitionStyle.Team(teamOne |> loadCompletedTeam, teamTwo |> loadCompletedTeam)
-            | CompletedCompetitionStyle.Individual completedQuizzers ->
-                completedQuizzers
-                |> List.map (fun q -> q.Name)
-                |> ItemizedCompetitionStyle.Individual }
+    { NumberOfQuestions =
+        quiz.CompletedQuestions.Length
+        |> PositiveNumber.numberOrOne
+      QuestionsWithEvents =
+        quiz.CompletedQuestions
+        |> List.indexed
+        |> List.collect (fun (i, v) ->
+            ItemizedScoreModel.refreshQuestionScore
+                (i + 1 |> PositiveNumber.numberOrOne)
+                ((QuizAnswer.Complete v.AnswerState), v.FailedAppeal))
+      CompetitionStyle =
+        match quiz.CompetitionStyle with
+        | CompletedCompetitionStyle.Team (teamOne, teamTwo) ->
+            ItemizedCompetitionStyle.Team(teamOne |> loadCompletedTeam, teamTwo |> loadCompletedTeam)
+        | CompletedCompetitionStyle.Individual completedQuizzers ->
+            completedQuizzers
+            |> List.map (fun q -> q.Name)
+            |> ItemizedCompetitionStyle.Individual }
 
 let update tryGetQuiz navigate (model: QuizDetailsModel) message =
     let navigateHomeCmd =
@@ -73,13 +72,19 @@ let update tryGetQuiz navigate (model: QuizDetailsModel) message =
         { model with Details = InProgress }, cmd
     | Initialize (Finished (Ok (Some quiz))) ->
         { model with
-            Details = Resolved
-                        (match quiz with
-                        | Running running -> { State = nameof Running; ItemizedScore = running |> loadRunningQuiz }
-                        | Completed completedQuiz ->  { State = nameof Completed; ItemizedScore = completedQuiz |> loadCompletedQuiz }
-                        | Official _ -> { State = nameof Official; ItemizedScore =Unchecked.defaultof<ItemizedScoreModel>}
-                    )
-                 },
+            Details =
+                Resolved(
+                    match quiz with
+                    | Running running ->
+                        { State = nameof Running
+                          ItemizedScore = running |> loadRunningQuiz }
+                    | Completed completedQuiz ->
+                        { State = nameof Completed
+                          ItemizedScore = completedQuiz |> loadCompletedQuiz }
+                    | Official _ ->
+                        { State = nameof Official
+                          ItemizedScore = Unchecked.defaultof<ItemizedScoreModel> }
+                ) },
         Cmd.none
     | Initialize (Finished (Ok None)) -> { model with Details = NotYetStarted }, navigateHomeCmd
     | Initialize (Finished (Error error)) -> model, navigateHomeCmd
@@ -101,11 +106,31 @@ let render dispatch (model: QuizDetailsModel) : Node =
             }
         }
     | Resolved loadedModel ->
-        concat {
-            p {
-                attr.``class`` "title"
-                $"Quiz {model.Code} Details" 
+        div {
+            attr.``class`` "column is-two-thirds"
+            div {
+                attr.``class`` "columns "
+
+                div {
+                    attr.``class`` "column "
+
+                    p {
+                        attr.``class`` "title"
+                        $"Quiz {model.Code} Details"
+                    }
+
+                }
+
+                div {
+                    attr.``class`` "column "
+
+                    span {
+                        attr.``class`` "tag is-success is-large"
+                        loadedModel.State
+                    }
+                }
             }
+
             render loadedModel.ItemizedScore dispatch
 
         }
